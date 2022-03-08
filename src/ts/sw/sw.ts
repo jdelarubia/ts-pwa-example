2065b7589eae9112b665c3f0b9d87ef4fe3f7aad
jdelarubia <jdelarubia@gmail.com>
2022-03-08 22:22:40 +0100

    sw add resources to be cached

---
+++ src/ts/sw/sw.ts
@@ -0,0 +1,9 @@
+const cacheVersion = 1;
+const cacheName = `PWAEasy-v${cacheVersion}`;
+const resources = [
+  "/",
+  "index.html",
+  "/js/bundle.js",
+  "https://fonts.googleapis.com/css2?&family=Open+Sans:wght@300&family=Roboto:wght@700&display=swap",
+];
+
