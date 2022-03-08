092fd3b97909c1af04abd05bb273e0f3984a17c5
jdelarubia <jdelarubia@gmail.com>
2022-03-08 22:30:02 +0100

    sw add fetch event listener

--- src/ts/sw/sw.ts
+++ src/ts/sw/sw.ts
@@ -16,3 +16,14 @@ self.addEventListener("install", async (event: Event) => {
     console.error(error);
   }
 });
+
+// cache-first then network
+self.addEventListener("fetch", async (event: any) => {
+  try {
+    const cachedResponse = await caches.match(event.request);
+    console.log(cachedResponse);
+    return cachedResponse || fetch(event.request);
+  } catch (error: any) {
+    console.error(error);
+  }
+});
