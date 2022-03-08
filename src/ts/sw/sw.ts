d3a1de3042b54e86f14de0ef90066545bc7b9bd4
jdelarubia <jdelarubia@gmail.com>
2022-03-08 22:22:54 +0100

    sw listen to install event

--- src/ts/sw/sw.ts
+++ src/ts/sw/sw.ts
@@ -7,3 +7,12 @@ const resources = [
   "https://fonts.googleapis.com/css2?&family=Open+Sans:wght@300&family=Roboto:wght@700&display=swap",
 ];
 
+self.addEventListener("install", async (event: Event) => {
+  console.log("[SW] install event fired!");
+  try {
+    const cache = await caches.open(cacheName);
+    return await cache.addAll(resources);
+  } catch (error: any) {
+    console.error(error);
+  }
+});
