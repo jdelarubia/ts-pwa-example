c5f3ca8d5c36bc3f11a84739406e51803189054e
jdelarubia <jdelarubia@gmail.com>
2022-03-15 19:33:07 +0100

    Create type of a web worker event

--- src/ts/sw/sw.ts
+++ src/ts/sw/sw.ts
@@ -7,7 +7,11 @@ const resources = [
   "https://fonts.googleapis.com/css2?&family=Open+Sans:wght@300&family=Roboto:wght@700&display=swap",
 ];
 
-self.addEventListener("install", async (event: Event) => {
+type WebWorkerEvent = Partial<
+  Event & { request?: Request; waitUntil?: Function }
+>;
+
+self.addEventListener("install", async (event: WebWorkerEvent) => {
   console.log("[SW] install event fired!");
   try {
     const cache = await caches.open(cacheName);
@@ -19,10 +23,9 @@ self.addEventListener("install", async (event: Event) => {
 });
 
 // cache-first then network as fallback
-self.addEventListener("fetch", async (event: any) => {
+self.addEventListener("fetch", async (event: WebWorkerEvent) => {
   try {
     const cachedResponse = await caches.match(event.request);
-    console.log(cachedResponse);
     return cachedResponse || fetch(event.request);
   } catch (error: any) {
     console.error(error);
