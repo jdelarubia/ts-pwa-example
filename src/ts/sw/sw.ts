da73d94c83551f7fd10556f055aa09e5f0317dec
jdelarubia <jdelarubia@gmail.com>
2022-03-15 21:52:38 +0100

    capitalize names of constants

--- src/ts/sw/sw.ts
+++ src/ts/sw/sw.ts
@@ -1,26 +1,28 @@
-import { WebWorkerEvent } from "../types";
+import { WebWorkerEvent } from "./types";
 
-const cacheVersion = 1;
-const cacheName = `PWAEasy-static-v${cacheVersion}`;
-const resources = [
+const CACHEVERSION = 2;
+const CACHENAME = `PWAEasy-static-v${CACHEVERSION}`;
+const RESOURCES = [
   "/",
   "index.html",
   "/js/bundle.js",
   "https://fonts.googleapis.com/css2?&family=Open+Sans:wght@300&family=Roboto:wght@700&display=swap",
 ];
 
+// install event
 self.addEventListener("install", async (event: WebWorkerEvent) => {
   console.log("[SW] install event fired!");
+  // console.log(event);
   try {
-    const cache = await caches.open(cacheName);
-    return await cache.addAll(resources);
+    const cache = await caches.open(CACHENAME);
+    return await cache.addAll(RESOURCES);
   } catch (error: any) {
     console.error(error);
   }
   return;
 });
 
-// cache-first then network as fallback
+// fetch event. Cache-first then network as fallback
 self.addEventListener("fetch", async (event: WebWorkerEvent) => {
   try {
     const cachedResponse = await caches.match(event.request);
