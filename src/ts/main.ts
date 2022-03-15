a675ea7b98607368530ad28e8f0ed6b91b947694
jdelarubia <jdelarubia@gmail.com>
2022-03-15 21:11:14 +0100

    type variable correctly

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -7,8 +7,9 @@ function registerSW() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", async () => {
       try {
-        const registration = await navigator.serviceWorker.register("/sw.js");
-        console.log("SW registered!");
+        const registration: ServiceWorkerRegistration =
+          await navigator.serviceWorker.register("/sw.js");
+        console.log("SW registered!", registration.scope);
       } catch (e: any) {
         console.error(e);
       }
