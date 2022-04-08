a6728140441338093fb84985e3c6c4ed76387718
jdelarubia <jdelarubia@gmail.com>
2022-04-08 22:57:44 +0200

    add event registration to the component itself

--- src/ts/add-to-home/addToHome.ts
+++ src/ts/add-to-home/addToHome.ts
@@ -4,26 +4,57 @@ template.innerHTML = `<span class="close-btn">&#10006;</span>
 <button class="install-btn">Yes</button>`;
 
 class AddToHome extends HTMLElement {
+  prompt: any;
+
   constructor() {
     super();
   } //.
 
   connectedCallback() {
     this.appendChild(template.content.cloneNode(true));
-    const btn = <HTMLButtonElement>this.querySelector("button");
-    btn.addEventListener("click", (ev: Event) => {
-      ev.preventDefault();
-      console.log(ev);
+    const closeBtn = <HTMLSpanElement>this.querySelector(".close-btn");
+
+    closeBtn.addEventListener("click", async (event: Event) => {
+      this.remove();
     });
-  }
+    this.registerBeforeInstallEvent();
+  } //.
 
   show() {
     this.classList.add("show");
-  }
+  } //.
 
   remove() {
     this.classList.remove("show");
-  }
+  } //.
+
+  private registerBeforeInstallEvent() {
+    window.addEventListener("beforeinstallprompt", async (event: Event) => {
+      event.preventDefault();
+      this.prompt = event;
+      this.show();
+      this.triggerInstallationPrompt();
+    });
+  } //.
+
+  private triggerInstallationPrompt() {
+    const addToHomeBtn = <HTMLButtonElement>this.querySelector(".install-btn");
+
+    addToHomeBtn.addEventListener("click", async (event: Event) => {
+      try {
+        const choice = await this.prompt.prompt();
+        console.log(choice);
+
+        if (choice.outcome === "accepted") {
+          console.log("User agreed to add button to screen");
+        }
+        this.prompt = null;
+        addToHomeBtn.remove();
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  } //.
 } //. AddToHome
 
 window.customElements.define("add-to-home", AddToHome);
