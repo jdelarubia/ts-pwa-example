7e2dfe22ec24da86f3ecb8648426b36e1851e05c
jdelarubia <jdelarubia@gmail.com>
2022-04-09 16:40:12 +0200

    move add-to-home styling to the component

--- src/ts/add-to-home/addToHome.ts
+++ src/ts/add-to-home/addToHome.ts
@@ -1,5 +1,46 @@
+const css = `<style>
+add-to-home {
+  display: flex;
+  align-items: center;
+  justify-self: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  background: var(--dark-blue);
+  border-radius: var(--border-radius);
+  margin: 0.3em 0;
+  opacity: 0;
+  overflow: hidden;
+  padding: 0.2em 0.5em;
+  transition: opacity 2s, visibility 2s;
+  visibility: hidden;
+}
+add-to-home span.close-btn {
+  width: 100%;
+  text-align: end;
+  color:white;
+  margin-bottom: 1em;
+}
+add-to-home button.install-btn {
+  color: white;
+  border: none;
+  border-radius: var(--border-radius);
+  background-color: var(--camel);
+  padding: 0.3rem;
+  font-size: large;
+  font-weight: bold;
+  height: calc(var(--btn-thumb-size) - 1.5rem);
+  width: var(--btn-thumb-size);
+}
+add-to-home.show {
+  visibility: visible;
+  opacity: 0.9;
+}
+</style>`;
 const template = document.createElement("template");
-template.innerHTML = `<span class="close-btn">&#10006;</span>
+template.innerHTML =
+  css +
+  `<span class="close-btn">&#10006;</span>
 <p>Add to home screen?</p>
 <button class="install-btn">Yes</button>`;
 
